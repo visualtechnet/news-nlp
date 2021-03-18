95259fe8ab27f6f8ef3092bac6299612d1f87d9e
Mark Guerrero <mark2579@gmail.com>
2021-03-17 20:52:31 -0400

    feat: upgraded webpack and webpack-cli to make workbox work

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,25 @@
 const path = require('path')
 const webpack = require('webpack')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+      rules: [
+        {
+          test: '/\.js$/',
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+      new WorkboxPlugin.GenerateSW(),
+      new HtmlWebPackPlugin({
+          template: "./src/client/views/index.html",
+          filename: "./index.html",
+      })
     ]
 }
