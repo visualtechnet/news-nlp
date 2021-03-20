9f9016f8d70644549304eb915af09f477face5c1
Mark Guerrero <mark2579@gmail.com>
2021-03-20 03:22:57 -0400

    feat: added test spect

---
+++ src/client/__test__/testNameChecker.spec.js
@@ -0,0 +1,6 @@
+import { checkForName } from '../js/nameChecker'
+
+describe("Testing the other functionality", () => {
+    test("Testing the namechecker() function", () => {
+        expect(checkForName).toBeDefined();
+})});
