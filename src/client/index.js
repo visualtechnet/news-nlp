ae6faca43142c2fde0631c8eedb75a9996c5c096
Mark Guerrero <mark2579@gmail.com>
2021-03-20 03:35:22 -0400

    feat: added simple validation

--- src/client/index.js
+++ src/client/index.js
@@ -8,7 +8,40 @@ import { handleSubmit } from './js/formHandler'
 
 
 const btnSubmit = document.querySelector("#btnSubmit");
+const errorMessage = document.querySelector('.errorMessage');
+
+btnSubmit.addEventListener('submit', function() {
+    const sentimentText = document.querySelector('#sentimentText').value;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (sentimentText.length === 0) {
+        errorMessage.innerHTML = "Enter sentiment Text";
+        errorMessage.classList.remove('hide');
+        errorMessage.classList.add('show');
+        return false;
+    } else {
+        handleSubmit()
+    }
+
+    return false;
+});
 
 btnSubmit.addEventListener('click', function() {
-    handleSubmit()
+    const sentimentText = document.querySelector('#sentimentText').value;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (sentimentText.length === 0) {
+        errorMessage.innerHTML = "Enter sentiment Text";
+        errorMessage.classList.remove('hide');
+        errorMessage.classList.add('show');
+        return false;
+    } else {
+        handleSubmit()
+    }
+
+    return false;
 });
